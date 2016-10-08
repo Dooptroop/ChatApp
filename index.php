e4bdc59a22e2aa358e49f944f8810349c0a5be66
mdupree <mdupree@acromediainc.com>
2016-10-08 12:39:08 -0700

    style and UI

--- index.php
+++ index.php
@@ -25,13 +25,29 @@ else{
       <p class="logout"><a id="exit" href="#">Exit Chat</a></p>
       <div style="clear:both"></div>
     </div>
-    <div id="chatbox"></div>
+    <div id="chatbox"><?php
+      if(file_exists("log/log.html") && filesize("log/log.html") > 0){
+        $handle = fopen("log/log.html", "r");
+        $contents = fread($handle, filesize("log/log.html"));
+        fclose($handle);
+
+        echo $contents;
+      }
+      ?>
+    </div>
 
     <form name="message" action="">
       <input name="usermsg" type="text" id="usermsg" size="63" />
       <input name="submitmsg" type="submit"  id="submitmsg" value="Send" />
     </form>
   </div>
+
+
+
+
+
+
+
   <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
   <script type="text/javascript" src="js/script.js"></script>
   <?php
